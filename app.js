e07a9ebd00d20afd4360decaef887ed92535fe62
Isha <isha.ojha@gmail.com>
2015-08-05 13:37:44 -0600

    project completed -- links remaining

--- app.js
+++ app.js
@@ -6,7 +6,16 @@ $(document).ready( function() {
 		var tags = $(this).find("input[name='tags']").val();
 		getUnanswered(tags);
 	});
+
+	$('.inspiration-getter').submit( function(event){
+	// zero out results if previous search has run
+	$('.results').html('');
+	// get the value of the tags the user submitted
+		var answerers = $(this).find("input[name='answerers']").val();
+		getTopAnswerers(answerers);
+	});
 });
+
 
 // this function takes the question object returned by StackOverflow 
 // and creates new result to be appended to DOM
@@ -61,10 +70,10 @@ var showError = function(error){
 var getUnanswered = function(tags) {
 	
 	// the parameters we need to pass in our request to StackOverflow's API
-	var request = {tagged: tags,
-								site: 'stackoverflow',
-								order: 'desc',
-								sort: 'creation'};
+	var request = { tagged: tags,
+				    site: 'stackoverflow',
+					order: 'desc',
+					sort: 'creation'};
 	
 	var result = $.ajax({
 		url: "http://api.stackexchange.com/2.2/questions/unanswered",
@@ -88,5 +97,83 @@ var getUnanswered = function(tags) {
 	});
 };
 
+///////////// GET INSPIRED FEATURE ////////////////
+
+
+/* User Object key values from Stack Overflow API -- 
+   http://api.stackexchange.com/docs/types/tag-score
+
+ "user": {
+    "reputation": 9001,
+    "user_id": 1,
+    "user_type": "registered",
+    "accept_rate": 55,
+    "profile_image": "https://www.gravatar.com/avatar/a007be5a61f6aa8f3e85ae2fc18dd66e?d=identicon&r=PG",
+    "display_name": "Example User",
+    "link": "http://example.stackexchange.com/users/1/example-user"
+  },
+  "post_count": 100,
+  "score": 20
+*/
+
+// this function takes the item object returned by StackOverflow 
+// and creates new result to be appended to DOM
+var showAnswerers = function(item) {
+	
+	// clone our result template code
+	var result = $('.templates .answerers').clone();
+	
+	// Set the user image in result
+	var userImage = result.find('.user-image');
+	userImage.attr('src', item.user.profile_image);
+
+	// Set the user name in the result
+	var userName = result.find('.user-name');
+	userName.attr('href', item.user.link)
+	userName.text(item.user.display_name);
+
+	// Set the user reputation in the result
+	var userReputation = result.find('.user-reputation');
+	userReputation.text(item.user.reputation);
+
+	// Set the user post count in result
+	var userPostCount = result.find('.user-postcount');
+	userPostCount.text(item.user.post_count);
+
+	// Set the user score in result
+	var userScore = result.find('.user-score');
+	userScore.text(item.user.score);
+
+	return result;
+};
+
+var getTopAnswerers = function(answerers) {
+	
+	// the parameters we need to pass in our request to StackOverflow's API
+	var request = { tag: answerers,
+				    site: 'stackoverflow'
+				};
+	
+	var result = $.ajax({
+		url: "http://api.stackexchange.com/2.2/tags/"+request.tag+"/top-answerers/all_time", // /tags/{tag}/top-answerers/{period}
+		data: request,
+		dataType: "jsonp",
+		type: "GET",
+		})
+	.done(function(result){
+		var searchResults = showSearchResults(request.tag, result.items.length);
+
+		$('.search-results').html(searchResults);
+
+		$.each(result.items, function(i, item) {
+			var answer = showAnswerers(item);
+			$('.results').append(answer);
+		});
+	})
+	.fail(function(jqXHR, error, errorThrown){
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	});
+};
 
 
